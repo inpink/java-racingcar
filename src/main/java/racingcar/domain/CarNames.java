54c7ce8324f83d7f7caf9cef04e80440fc2cb144
inpink <inpink@kakao.com>
2024-01-17 04:37:55 +0900

    feat: 1개 이상의 자동차 이름을 입력

--- src/main/java/racingcar/domain/CarNames.java
+++ src/main/java/racingcar/domain/CarNames.java
@@ -3,10 +3,14 @@ package racingcar.domain;
 import java.util.List;
 
 public class CarNames {
+    private final static int MIN_COUNT = 1;
+    private final static String INVALID_NAMES_COUNT = "올바르지 않은 자동차 개수입니다.";
+
 
     private final List<CarName> carNames;
 
     private CarNames(List<CarName> carNames) {
+        validate(carNames);
         this.carNames = carNames;
     }
 
@@ -16,6 +20,16 @@ public class CarNames {
                 .toList();
 
         return new CarNames(convertedCarNames);
+    }
+
+    private void validate(List<CarName> carNames) {
+        validateSize(carNames);
+    }
+
+    private void validateSize(List<CarName> carNames) {
+        if (carNames.size() < MIN_COUNT) {
+            throw new IllegalArgumentException(INVALID_NAMES_COUNT);
+        }
     }
 
     public List<String> getCarNames() {
---
+++ src/test/java/racingcar/domain/CarNamesTest.java
@@ -0,0 +1,49 @@
+package racingcar.domain;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarNamesTest {
+
+    @ParameterizedTest
+    @MethodSource("validCarNamesProvider")
+    void create_withValidNumberOfNames(List<String> names) {
+        // when
+        CarNames carNames = CarNames.create(names);
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            assertNotNull(carNames);
+            assertEquals(names.size(), carNames.getCarNames().size());
+        });
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidCarNamesProvider")
+    void create_withInvalidNumberOfNames(List<String> names) {
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> CarNames.create(names));
+    }
+
+    static Stream<List<String>> validCarNamesProvider() {
+        return Stream.of(
+                List.of("Car1"),
+                List.of("Car1", "Car2"),
+                List.of("A", "B", "C", "D", "E")
+        );
+    }
+
+    static Stream<List<String>> invalidCarNamesProvider() {
+        return Stream.of(
+                List.of(),
+                List.of(" ", " ")
+        );
+    }
+
+
+}

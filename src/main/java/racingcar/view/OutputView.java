769d04e58e857381c598e8b5318391ac950ca3d0
inpink <inpink@kakao.com>
2024-01-17 06:13:11 +0900

    feat: 레이싱 게임 결과 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,10 +8,16 @@ import racingcar.domain.TryCount;
 public class OutputView {
 
     public void outputWinners(List<Car> winners) {
-
+        winners.stream()
+                .forEach(winner -> System.out.println("우승자: " + winner.getCarName().getName()));
     }
 
     public void outputPositions(TryCount tryCount, Cars cars) {
-
+        for (int index = 0; index < tryCount.getCount(); index++) {
+            final int finalIndex = index;
+            cars.getCars().stream()
+                    .forEach(car -> System.out.println(car.getCarName().getName() + car.getPosition(finalIndex)));
+            System.out.println();
+        }
     }
 }
